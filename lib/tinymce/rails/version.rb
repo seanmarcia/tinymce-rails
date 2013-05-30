66c32d223fb91c291005c92221bed1417374b78b
Sean Marcia <sean@cs.uoregon.edu>
2013-05-30 10:19:03 -0400

    Initial commit

---
+++ lib/tinymce/rails/version.rb
@@ -0,0 +1,6 @@
+module TinyMCE
+  module Rails
+    VERSION = "3.5.8.2"
+    TINYMCE_VERSION = "3.5.8"
+  end
+end
---
+++ spec/lib/engine_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+module TinyMCE::Rails
+  describe Engine do
+    describe ".default_base" do
+      it "generates a default path based on the asset prefix" do
+        TinyMCE::Rails::Engine.default_base.should eq "/assets/tinymce"
+      end
+
+      it "ignores the asset prefix if missing" do
+        Rails.application.config.assets.stub!(:prefix).and_return nil
+        TinyMCE::Rails::Engine.default_base.should eq "/tinymce"
+      end
+
+      it "includes the Rails relative_url_root if provided" do
+        Rails.application.config.stub!(:relative_url_root).and_return "/prefix"
+        TinyMCE::Rails::Engine.default_base.should eq "/prefix/assets/tinymce"
+      end
+    end
+  end
+end
